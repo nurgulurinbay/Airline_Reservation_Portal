e8be975d5dd94806ae6585a23b1c11481fc81938
nurgulurinbay <253455@astanait.edu.kz>
2026-01-18 22:41:18 +0500

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USER = "postgres"; // your username
-    private static final String PASSWORD = "your_password"; // your password
+    private static final String USER = "postgres"; 
+    private static final String PASSWORD = "your_password"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
